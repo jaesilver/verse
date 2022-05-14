21d8698b8dc086013189cbe464286cdd72b9d193
Jeffrey Shijo <96687626+jeffreyshijo@users.noreply.github.com>
2022-05-14 21:53:06 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Donation made to which of the following institutes is allowed as deduction under Sec. 80G?",
+    answer: "All of these",
+    options: [
+      "National Defence Fund",
+      "PM Drought Relief Fund",
+      "PM National Relief Fund",
+      "All of these"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Your father has a house property which he has given on rent. His rental income shall be chargeble to tax under the head income from _______",
+    answer: "House property",
+    options: [
+      "Salary",
+      "Business",
+      "House property",
+      "Other sources"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Gratuity received from employer is taxable under the head ________",
+    answer: "Income from salaries",
+    options: [
+      "Income from salaries",
+      "Income from Business/ Profession",
+      "Income from Other Sources",
+      "ncome from House Property"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is/are dealt in ICDS-IV?",
+    answer: "Option 1 and 2",
+    options: [
+      "Revenue from sale of goods",
+      "Revenue from sale of rendering services",
+      "Option 1 and 2",
+      "None of these"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does TIN stand for?",
+    answer: "Tax Information Network",
+    options: [
+      "Table index number",
+      "Tax Idiology Network",
+      "Tax Information Network",
+      "Tax Index Number"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
